70bc77fbd8d21bfbec1738e37a08f052b717fea3
Theee Sheeep <79360988+thatonesheep80@users.noreply.github.com>
2021-10-25 17:39:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+rightWristY=0;
+mainText = document.getElementById("main_TextInput").value;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -12,7 +19,11 @@ function setup()
 
 function draw()
 {
-    background('#18ba43');
+    background('#969A97');
+    fill('#F90093');
+    stroke('#F90093');
+    textSize(difference);
+    text(mainText, noseX, noseY);
 }
 
 function modelLoaded()
@@ -25,5 +36,14 @@ function gotPoses(results)
     if (results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + ", rightWristX = " + rightWristX);
     }
 }
